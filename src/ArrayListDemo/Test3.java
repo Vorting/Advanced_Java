fd147558537bbb1a8b4fb4b60c8e4ff51139cc84
Dima Ivanov <ya.warkus@gmail.com>
2019-08-29 11:57:41 +0300

    initial release

---
+++ src/ArrayListDemo/Test3.java
@@ -0,0 +1,18 @@
+package ArrayListDemo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Test3 {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+
+        for (int i = 0; i < 10; i++) {
+            list.add(i);
+        }
+
+        list.remove(5);
+        System.out.println(list);
+
+    }
+}
