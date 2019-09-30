d69b6107693af73f52187d8e0ea0af2dd414e330
Dima Ivanov <ya.warkus@gmail.com>
2019-09-30 17:24:57 +0300

    add simple example ExcDemo1

--- src/shildt/edition5/chapter9/practice/theory/ExcDemo1.java
+++ src/shildt/edition5/chapter9/practice/theory/ExcDemo1.java
@@ -2,6 +2,18 @@ package shildt.edition5.chapter9.practice.theory;
 
 public class ExcDemo1 {
     public static void main(String[] args) {
+        int[] nums = new int[4];
 
+        //Создание блока try.
+        try {
+            System.out.println("Before exception is generated.");
+            //попытка обратиться за границы массива.
+            nums[7] = 10;
+            System.out.println("this won`t be displayed");
+            //перехват исключения в связи с обращением за границы массива
+        } catch (ArrayIndexOutOfBoundsException exc) {
+            System.out.println("Index out-of-bounds!");
+        }
+        System.out.println("After catch statement.");
     }
 }
