ee330084a54c8ae0af17d33fdb6458d6f9abfbc3
Dima <ya.warkus@gmail.com>
2019-10-21 21:10:32 +0300

    creat 2 classes in LinkedList

---
+++ src/advanced_Java/LinkedList/Test3.java
@@ -0,0 +1,11 @@
+package advanced_Java.LinkedList;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Test3 {
+    public static void main(String[] args) {
+        List<Integer> list = new LinkedList<>();
+
+    }
+}
