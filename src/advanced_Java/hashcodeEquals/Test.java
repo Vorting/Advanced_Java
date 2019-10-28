35e0ab36aa9da67fa004bc6c17a09a4cfae4adb6
Dima Ivanov <ya.warkus@gmail.com>
2019-10-28 17:24:03 +0200

    add package hashCodeEquals

---
+++ src/advanced_Java/hashcodeEquals/Test.java
@@ -0,0 +1,45 @@
+package advanced_Java.hashcodeEquals;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class Test {
+    public static void main(String[] args) {
+        Map<Person, String> map = new HashMap<>();
+        Set<Person> set = new HashSet<>();
+
+        Person person1 = new Person(1, "Mike");
+        Person person2 = new Person(1, "Mike");
+
+        map.put(person1,"123");
+        map.put(person2,"123");
+
+        set.add(person1);
+        set.add(person2);
+
+        System.out.println(map);
+        System.out.println(set);
+    }
+}
+
+class Person {
+    private int id;
+    private String name;
+
+    public Person(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+}
+
