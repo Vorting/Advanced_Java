5a222a53d89d23cc5144e81db1232f02e3f2706a
Dima Ivanov <ya.warkus@gmail.com>
2019-10-28 17:37:00 +0200

    example of hashCodeEquals

--- src/advanced_Java/hashcodeEquals/Test.java
+++ src/advanced_Java/hashcodeEquals/Test.java
@@ -40,6 +40,23 @@ class Person {
                 ", name='" + name + '\'' +
                 '}';
     }
+    
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
 
+        Person person = (Person) o;
+
+        if (id != person.id) return false;
+        return name != null ? name.equals(person.name) : person.name == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        return result;
+    }
 }
 
