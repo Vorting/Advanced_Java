5b61c3c54a6ff0630fda26189a4a2134857808b7
Dima Ivanov <ya.warkus@gmail.com>
2019-10-29 16:30:57 +0200

    add 2nd example of ListIterator

--- src/advanced_Java/set/TestIterator2.java
+++ src/advanced_Java/set/TestIterator2.java
@@ -1,12 +1,17 @@
-package advanced_Java;
+package advanced_Java.set;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.ListIterator;
 
-public class TestIterator {
+public class TestIterator2 {
+    private static int increase = 0;
+//    private static int decrease = 0;
+
     public static void main(String[] args) {
 
+        int count = increase;
+        int count2 = increase;
         try {
             List<Integer> arrayList = new ArrayList<>();
 
@@ -19,15 +24,25 @@ public class TestIterator {
 
             ListIterator<Integer> listIterator = arrayList.listIterator();
 
-            Object current = arrayList.listIterator(1);
-            Object prev = arrayList.listIterator(0);
+            int currentIdx = listIterator.next();
+            int nextIndex = listIterator.nextIndex();
             System.out.println("\nUsing ListIterator:\n");
             while (listIterator.hasNext()) {
+                if (currentIdx!=nextIndex) {
+                    count++;
+                    currentIdx = listIterator.next();
+                    nextIndex = listIterator.next();
+                    continue;
+                } else if (currentIdx==nextIndex){
+                    count2++;
+                }
+
                 System.out.println("Value is: " + listIterator.next());
 
             }
         } catch (NullPointerException e) {
-            System.out.println("Exception trown : " + e);
+            System.out.println("Exception thrown : " + e);
         }
+        System.out.println(count2);
     }
 }
