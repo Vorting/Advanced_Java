973845d10494c594e5cd63cce875f224fc68e4dd
Dmitry <ya.warkus@gmail.com>
2020-02-12 17:26:54 +0200

    fix

--- src/advanced_Java/comparator/ComparatorTest.java
+++ src/advanced_Java/comparator/ComparatorTest.java
@@ -1,7 +1,19 @@
 package advanced_Java.comparator;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class ComparatorTest {
     public static void main(String[] args) {
-        
+        List<String> animals = new ArrayList<>();
+        animals.add("dog");
+        animals.add("cat");
+        animals.add("frog");
+        animals.add("bird");
+
+        Collections.sort(animals);
+
+        System.out.println(animals);
     }
 }
